cfa372683726610d1255e90056b8ca5cf1597f99
laghzal <laghzaltarik0@gmail.com>
2025-08-21 00:19:23 +0100

    1

--- 1337/libft/ft_strnstr.c
+++ 1337/libft/ft_strnstr.c
@@ -1,71 +1,35 @@
-/*#include <string.h>
-
-char *ft_strnstr(const char *word,const char *tofind,size_t len)
-{
-	size_t	i;
-	size_t	j;
-
-	i = 0;
-	if (!tofind[0])
-		return (char *)word;
-	while (word[i] && i < len)
-	{
-#include "libft.h"
-
-		j = 0;
-		while ((i + j) < len && word[i + j] == tofind[j] && tofind[j])
-		{
-			if (tofind[j + 1] == '\0')
-				return (char *)&word[i];
-			j++;
-		}
-		i++;
-	}
-	return (NULL);
-}
-#include <stdio.h>
-int main()
-{
-	char *s = "hello world";
-	printf("%s\n",ft_strnstr(s,"world",10));
-	printf("%s\n",ft_strnstr(s,"",11));
-	printf("%s\n",ft_strnstr("abcdef","de",6));
-	printf("%s\n",ft_strnstr(s,"world",5));
-	return 0;
-}
-*/
 #include <string.h>
 #include <stdio.h>
+#include "libft.h"
 
-char *ft_strnstr(const char *word,const char *tofind,size_t len)
+char *ft_strnstr(const char *word, const char *tofind, size_t len)
 {
     size_t i;
     size_t j;
-
+    size_t tofind_len;
+    
     if (!tofind[0])
         return (char *)word;
+    tofind_len = 0;
+    while (tofind[tofind_len])
+        tofind_len++;
+    
     i = 0;
     while (word[i] && i < len)
     {
-	printf("i = %zu\n", i);
+        if (i + tofind_len > len)
+            break;
+            
         j = 0;
-        while ((i + j) < len && word[i + j] == tofind[j] && tofind[j])
+        while (j < tofind_len && word[i + j] == tofind[j])
         {
-            if (tofind[j + 1] == '\0')
-                return (char *)&word[i];
             j++;
         }
+        
+        if (j == tofind_len)
+            return (char *)&word[i];
+            
         i++;
     }
     return NULL;
 }
-
-int main(void)
-{
-    char *s = "hello world";
-    printf("%s\n", ft_strnstr(s,"",11));
-    printf("%s\n", ft_strnstr("abcdef","de",6));
-    printf("%s\n", ft_strnstr(s,"world",5));
-    return 0;
-}
-
