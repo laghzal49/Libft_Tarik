8a7c5fdc79aabf2aafa2dc6f0e97601f3174beec
laghzal <laghzaltarik0@gmail.com>
2025-08-24 02:05:20 +0100

    a

--- 1337/libft/ft_strrchr.c
+++ 1337/libft/ft_strrchr.c
@@ -1,31 +1,18 @@
 #include <string.h>
 #include "libft.h"
-int	ftt_strlen(char const *s)
-{
-	int	i;
-	
-	i = 0;
-	while(*s)
-	{
-		s++;
-		i++;
-	}
-
-	return (i);
-}
-
 char *ft_strrchr(const char *s, int c)
 {
-    char find = (char)c;
-    size_t len = ftt_strlen(s);
+    char *last = NULL;
+    char ch = (char)c;
 
-    while (1)
+    while(*s)
     {
-        if (s[len] == find)
-            return (char *)&s[len];
-        if (len == 0)
-            break;
-        len--;
+        if (*s == ch)
+            last = (char *)s;
+        s++;
     }
-    return NULL;
+    if (ch == '\0')
+        return (char *)s;
+    return last;
 }
+
