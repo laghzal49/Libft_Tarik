2c61e04a1b0afa4c47a588eb9be8b19d58a95834
laghzal <laghzaltarik0@gmail.com>
2025-08-25 19:14:14 +0100

    ft_memcpy

--- 1337/libft/ft_memcpy.c
+++ 1337/libft/ft_memcpy.c
@@ -6,28 +6,26 @@
 /*   By: laaghzal <laaghzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/21 17:40:28 by laaghzal          #+#    #+#             */
-/*   Updated: 2025/08/24 18:50:45 by laaghzal         ###   ########.fr       */
+/*   Updated: 2025/08/25 19:13:52 by laaghzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
+#include <string.h>
 #include "libft.h"
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	char	*p;
-	char	*s;
-	size_t	i;
+	unsigned char	*p;
+	unsigned char	*s;
 
-	p = (char *)dest;
-	s = (char *)src;
-	if (!src || !dest)
+	if (!dest && !src)
 		return (NULL);
-	i = 0;
-	while (i < n)
+	p = (unsigned char *)dest;
+	s = (unsigned char *)src;
+	while (n)
 	{
-		p[i] = s[i];
-		i++;
+		*(p++) = *(s++);
+		n--;
 	}
 	return (dest);
 }
