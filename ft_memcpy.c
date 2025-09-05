4ff13cfbaba1211ffebf439b93e8f6319f5d3ebd
tlaghzal <laghzaltarik0@gmail.com>
2025-09-05 10:15:20 +0100

    feat(memory): implement ft_memcpy buffer copying utility
    Implement linear memory copying between non-overlapping source and destination
    buffers.
    
    - Copies exactly n bytes from memory area src to dest.
    - Returns original dest pointer as per standard libc specification.
    - Guards against NULL pointer combinations when n > 0.

---
+++ ft_memcpy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlaghzal <tlaghzal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/14 14:51:57 by tlaghzal          #+#    #+#             */
+/*   Updated: 2025/10/21 12:39:01 by tlaghzal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char		*d;
+	const unsigned char	*s;
+
+	if (!dest && !src)
+		return (NULL);
+	d = (unsigned char *)dest;
+	s = (unsigned char *)src;
+	if (n == 0)
+		return (dest);
+	while (n--)
+		*d++ = *s++;
+	return (dest);
+}
