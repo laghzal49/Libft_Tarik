6ecf21069289fee3e6b4dcce6cfbd164f7c29c71
tlaghzal <laghzaltarik0@gmail.com>
2025-09-07 10:15:20 +0100

    feat(memory): implement ft_memmove supporting overlapping memory buffers
    Handle memory region copies where source and destination memory blocks overlap,
    safely preventing destination corruption during copy traversal.
    
    - Inspects pointer addresses to determine forward vs backward copying direction.
    - Prevents byte clobbering when dest > src within the same contiguous memory block.

---
+++ ft_memmove.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlaghzal <tlaghzal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/14 15:18:57 by tlaghzal          #+#    #+#             */
+/*   Updated: 2025/10/19 11:55:13 by tlaghzal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char		*d;
+	const unsigned char	*s;
+
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
+	if (n == 0 || dest == src)
+		return (dest);
+	if (d < s)
+	{
+		while (n--)
+			*d++ = *s++;
+	}
+	else
+	{
+		d += n - 1;
+		s += n - 1;
+		while (n--)
+			*d-- = *s--;
+	}
+	return (dest);
+}
