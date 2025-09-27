093ef187a92c7950ba35fcdf290b44dedfd54a0b
tlaghzal <laghzaltarik0@gmail.com>
2025-09-27 10:15:20 +0100

    feat(part2): implement string concatenation in ft_strjoin
    Allocate fresh memory and return new string resulting from concatenation of s1 and s2.
    
    - Computes combined length of both input strings.
    - Allocates combined buffer and sequentially copies characters.
    - Returns NULL if allocation fails or inputs are invalid.

---
+++ ft_strjoin.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlaghzal <tlaghzal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/17 10:37:15 by tlaghzal          #+#    #+#             */
+/*   Updated: 2025/10/17 18:57:29 by tlaghzal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	size_t	len1;
+	size_t	len2;
+	char	*ptr;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	ptr = malloc(len1 + len2 + 1);
+	if (!ptr)
+		return (NULL);
+	ft_memcpy(ptr, s1, len1);
+	ft_memcpy(ptr + len1, s2, len2);
+	ptr[len1 + len2] = '\0';
+	return (ptr);
+}
