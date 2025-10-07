b945bdd5ecd12b2beb13229b7b38fbbc223a5768
tlaghzal <laghzaltarik0@gmail.com>
2025-10-07 10:15:20 +0100

    feat(part2): implement functional mapping helpers ft_strmapi and ft_striteri
    Implement higher-order functional utilities applying function pointer f to
    each character of string s.
    
    - ft_strmapi: creates new string resulting from successive applications of f.
    - ft_striteri: applies f in-place to each character passing index and pointer.

---
+++ ft_strmapi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlaghzal <tlaghzal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/17 18:34:15 by tlaghzal          #+#    #+#             */
+/*   Updated: 2025/10/19 11:59:38 by tlaghzal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(const char *s, char (*f)(unsigned int, char))
+{
+	char			*result;
+	unsigned int	i;
+
+	i = 0;
+	if (!s || !f)
+		return (NULL);
+	result = malloc(ft_strlen(s) + 1);
+	if (!result)
+		return (NULL);
+	while (i < ft_strlen(s))
+	{
+		result[i] = (*f)(i, s[i]);
+		i++;
+	}
+	result[i] = 0;
+	return (result);
+}
