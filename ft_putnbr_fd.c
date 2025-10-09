3e4f6da5d7fe1a7ca3cf8c1f61e11031d8389831
tlaghzal <laghzaltarik0@gmail.com>
2025-10-09 10:15:20 +0100

    feat(io): implement file descriptor output primitives ft_putchar_fd, ft_putstr_fd, ft_putendl_fd, ft_putnbr_fd
    Implement low-level character, string, and integer output routines directing
    writes to specified file descriptors.
    
    - ft_putchar_fd: writes single character using write system call.
    - ft_putstr_fd: writes complete null-terminated string to specified fd.
    - ft_putendl_fd: writes string followed by newline character '\n'.
    - ft_putnbr_fd: recursively decomposes and prints signed integer to fd.

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlaghzal <tlaghzal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/16 22:01:01 by tlaghzal          #+#    #+#             */
+/*   Updated: 2025/10/19 11:56:51 by tlaghzal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	int	num;
+
+	if (n == -2147483648)
+		write(fd, "-2147483648", 11);
+	else if (n < 0)
+	{
+		write(fd, "-", 1);
+		n = -n;
+		ft_putnbr_fd(n, fd);
+	}
+	else
+	{
+		if (n > 9)
+		{
+			ft_putnbr_fd(n / 10, fd);
+			ft_putnbr_fd(n % 10, fd);
+		}
+		else
+		{
+			num = n + '0';
+			write(fd, &num, 1);
+		}
+	}
+}
